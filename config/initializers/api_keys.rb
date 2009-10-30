cd4d15a2a0c8ff9aa48dee0eb87783fdb6a23141
Jan Krutisch <jan@krutisch.de>
2009-10-30 14:58:13 +0100

    api-keys heroku style

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,3 +1,8 @@
 class SiteConfig
-  API_KEYS = YAML.load_file(File.join(Rails.root, "config", "api_keys.yml"))
+  if File.exists?(File.join(Rails.root, "config", "api_keys.yml"))
+    api_keys = YAML.load_file(File.join(Rails.root, "config", "api_keys.yml"))
+    ECHONEST_KEY = api_keys['echonest']
+  else
+    ECHONEST_KEY = ENV['ECHONEST_API_KEY']
+  end
 end
