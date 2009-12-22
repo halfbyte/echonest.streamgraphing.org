ef09f434765c4eb3779257a7860972ebecc71ea7
Jan Krutisch <jan@krutisch.de>
2009-12-22 16:05:01 +0100

    watch your abbruchbedingungen

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -1,7 +1,7 @@
 class FeedItem < ActiveRecord::Base
-  
+
   ECHONEST_API_VERSION = "3"
-  
+
   include HTTParty
   base_uri 'developer.echonest.com/api/'
 
@@ -11,41 +11,39 @@ class FeedItem < ActiveRecord::Base
 
   def self.data_for_streamgraph(start_date = nil)
     min_date = FeedItem.minimum(:published_on)
-    
+
     min_date = start_date if start_date && min_date < start_date
-    
+
     max_date = FeedItem.maximum(:published_on)
-    
+
     min_year = min_date.year
     min_week = min_date.cweek
-    
+
     max_year = max_date.year
     max_week = max_date.cweek
-    
-    puts "from year: #{min_year} / cw: #{min_week}"
-    puts "to   year: #{max_year} / cw: #{max_week}"
-    
+
+
     current_week = 0
-    
+
     data = []
     weeks = []
     self.hottt_artists.length.times { data << [] }
-    
+
     loop do
       year = min_year + ((min_week + current_week) / 53)
       cw = (min_week + current_week) % 53
-      break if (year >= max_year && cw > max_week)
-      
+
+
       items = FeedItem.find_all_by_pub_year_and_pub_cw(year, cw)
-      
+
       items_per_artist = {}
-      
+
       items.each do |item|
         if artists.include?(item.artist)
           items_per_artist[item.artist] = (items_per_artist[item.artist] ? items_per_artist[item.artist] + 1 : 1)
         end
       end
-      
+
       self.hottt_artists.length.times { |i| data[i] << 0 }
       items_per_artist.each do |artist, count|
         if self.hottt_artists.include?(artist)
@@ -54,12 +52,13 @@ class FeedItem < ActiveRecord::Base
       end
       weeks[current_week] = "CW#{cw} #{year}"
       current_week += 1
-      
+      break if (year >= max_year && cw >= max_week)
+
     end
     # self.artists.length.times { |i| data[i] << 0 }
     # self.artists.length.times { |i| data[i] << 0 }
     [weeks, data]
-    
+
   end
 
   def self.hottt_artists
@@ -92,7 +91,7 @@ class FeedItem < ActiveRecord::Base
           end
         end
       end
-      
+
     end
   end
 end
