236f3db6c8c1879bfe8276253c9a2f10a994b4c4
Jan Krutisch <jan@krutisch.de>
2012-03-12 17:15:09 +0100

    fixing

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'feedtools', :lib => 'feed_tools'
-  # config.gem 'rest-client', :lib => 'rest_client'
-  config.gem 'httparty'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
